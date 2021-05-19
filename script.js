1a3cf312af3949490ac016c420ce18b96454577d
tux <fb9@protonmail.com>
2021-05-19 10:41:44 +0400

    update wiki objets

---
+++ script.js
@@ -0,0 +1,39 @@
+
+/* FONCTION ALEATOIRE */
+// function aleatoire(max, min){
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+
+// }
+
+// document.getElementById('p1').innerHTML = aleatoire(3, 1);
+
+// let prez = document.getElementById('p2').innerHTML = 'Bonjour je suis pierre';
+
+// let prez2 = prez.replace('pierre', '<b>mathilde<b>');
+// document.getElementById('p2').innerHTML = prez2;
+
+/* FONCTION ANONYME */
+
+// let maVariable = function(){  //Création de la fonction anonyme dans une variable
+//     alert('message d\'une fonction anonyme !'); 
+// }
+
+// maVariable(); //Appel de la variable
+
+/* FONCTION RECURSIVES */
+
+// function decompte(t){
+//     if(t > 0){
+//         document.getElementById('p1').innerHTML += t +' ';
+//         return decompte(t -1);
+//     }
+//         else{
+//             return t;
+//         }
+    
+// };
+
+// decompte(10);
+
+
+
