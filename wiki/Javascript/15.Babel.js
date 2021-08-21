6c710601a8d08263bffbd530d70565d9ec9967b2
koss <dev.flo@protonmail.com>
2021-08-21 10:34:08 +0400

    update final

--- wiki/Javascript/15.Babel.js
+++ wiki/Javascript/15.Babel.js
@@ -18,3 +18,42 @@ NodeJS embarque un gestionnaire de dépendances (npm) qui permet d'installer bab
     *Babel
     - Se rendre sur babeljs.io puis setup et choisir CLI
 
+    *Ouvrir un CLI (admin) et se placer dans le répertoire du projet
+    > npm init -y //Va créer un fichier package.json
+
+    *Installation des dépendances (copié à partir du site babel)
+    > npm install --save-dev @babel/core @babel/cli 
+    
+     - @babel/core : le coeur de babel pour son fonctionement
+     - @babel/cli  : pour utiliser babel en CLI
+     - @babel/preset-env : permet de compiler le code JS en code compatible avec les anciens navigateur (norme EMASCRIPT post 2015)
+     - babel-preset-minify : permet d'optimiser le code, utile sur un serveur de production.
+
+     *les dépendances seront ajoutées au fichier package.json
+
+     *Créer un nouveau dossier js dans le projet pour compiler le code
+      On indique une commande dans le fichier package.json après scripts: {
+      "build": "babel src/js -d js" //src/js = dosier source et js dosier contenant les fichiers compilés
+     
+    *Dans le CLI on compile 
+     > npm run build
+     
+     - Un nouveau dossier js sera créé avec une copie des fichiers sans aucune modification
+     - Il faudra indiqué à babel comment on veut compilé les fichiers
+     - On cré un fichier .babelrc à la racine du projet et on entre les informations
+    {
+    "presets": ["@babel/preset-env"]
+    }
+    
+    *On compile
+    > npm run build
+    - Le code est maintenant compilé pour les anciens navigateurs
+
+    *On optimise maintenant le code avec minify
+    {
+    "presets": ["@babel/preset-env", "minify"]
+    }
+
+    *On compile
+    > npm run build
+    - Le code est compressé, illisible mais le téléchargement sera plus rapide et optimisé sur un serveur en production.
---
+++ wiki/Javascript/16.Polyphill.js
@@ -0,0 +1,10 @@
+/* Les polyphill
+
+Un polyfill est un bout de code (généralement en JavaScript sur le web) utilisé pour fournir
+ des fonctionnalités récentes sur d'anciens navigateurs qui ne les supportent pas nativement.
+
+Une recherche sur google en tapant : polyphill + la fonctionnalité pour la quel on veut trouver
+un polyphill
+
+ex : polyphill promise
+
